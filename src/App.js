54587e421e4dcbdb5de10c5866497cfac1da2643
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-05-22 12:06:26 +0300

    add redux toolkit

--- src/App.js
+++ src/App.js
@@ -7,12 +7,31 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import NotFoundBlock from './pages/NotFound';
 import Cart from './pages/Cart';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from './redux/filter/slice';
 
 function App() {
   const [searchValue, setSearchValue] = React.useState('')
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
 
   return (
     <div className="wrapper">
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
       <Header searchValue={searchValue} setSearchValue={setSearchValue} />
       <div className="content">
         <div className="container">
